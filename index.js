7df6f819a1b251aff0520cd9908843dc501b18a2
VincentChisaka <vincentchisaka2@gmail.com>
2022-11-24 13:46:52 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Vincent Chisaka';
+const welcomeMessage = 'Welcome to Flatbook, Vincent Chisaka!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, VINCENT CHISAKA!';
+const shortGreeting = 'Welcome, V!'
